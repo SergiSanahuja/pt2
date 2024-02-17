df2e65de0e119215d7c65fb772418f2cd53316ee
Sergi Sanahuja <80123937+SergiSanahuja@users.noreply.github.com>
2024-02-17 18:25:11 +0100

    Canviar de html a php

--- View/material.vista.php
+++ View/material.vista.php
@@ -10,7 +10,7 @@
      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
      <script src="https://code.jquery.com/jquery-3.6.4.js" integrity="sha256-a9jBBRygX1Bh5lt8GZjXDzyOB+bWve9EiO7tROUtj/E=" crossorigin="anonymous"></script>
-
+ 
      <!-- CSS -->
      <link rel="stylesheet" href="../Assets/Css/material.style.css">
 </head>
@@ -56,8 +56,8 @@
                     </ul>
                 </div><br>
                 <div>
-                    <label for="nomMaterial">Nom material</label>
-                    <input type="text" id="nomMaterial" name="nomMaterial">
+                    
+                    <input type="text" id="nomMaterial" name="nomMaterial" aria-label="Nom material" placeholder="Nom Material">
                     <input type="number" id="quantitatMaterial" name="quantitatMaterial" value="1" min="1">
                     <label for='arxiuPujat' class='btn btn-primary'>Search...</label>
                     <input id='arxiuPujat' type="file" class="btn btn-primary">
@@ -66,7 +66,7 @@
                     <button type="button" class="btn btn-primary" id="eliminarMaterial">-</button>
                 </div>
                 <!--DIV -->
-                <div class="container  d-flex flex-wrapgr">
+                <div class="container d-flex flex-wrapgr">
                     <!--Cards-->
                     <div class="card m-2" style="width: 18rem;">
                         <img src="../Assets/img/material.jpg" class="card-img-top" alt="...">

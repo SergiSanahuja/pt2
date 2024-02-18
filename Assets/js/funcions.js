649b2838c6c2eb768ab28bb60fd059ad8018645a
Sergi Sanahuja <80123937+SergiSanahuja@users.noreply.github.com>
2024-02-18 17:35:50 +0100

    commit

--- Assets/js/funcions.js
+++ Assets/js/funcions.js
@@ -1,3 +1,19 @@
+
+var fs = require('fs');
+var XLSX = require('xlsx');
+var wb = XLSX.utils.book_new();
+wb.Props = {
+    Title: "FileFomat",
+    Subject: "Developer Guide"
+};
+wb.SheetNames.push("Test Sheet");
+var ws_data = [['hello' , 'world']];
+var ws = XLSX.utils.aoa_to_sheet(ws_data);
+wb.Sheets["Test Sheet"] = ws;
+var wbout = XLSX.write(wb, {bookType:'xlsx', type: 'binary'});
+
+
+
 function init() {
     $('#afegirForm').click(() => {
         // $('#form')[0].reset();
@@ -9,4 +25,8 @@ function init() {
     });
 }
 
-window.onload = init;
+
+
+
+
+init();

5261f7aa0028d41da9b8a0dd8545064a6fd1c110
AlbeX <a.morcillo@sapalomera.cat>
2024-03-11 16:06:24 +0100

    codigo php

---
+++ Assets/js/funcions_tallers.js
@@ -0,0 +1,26 @@
+
+
+$(document).ready(function(){
+    $('#afegirTaller').click(function(e){
+      e.preventDefault();
+      let formData = new FormData($('#form')[0]);
+      $.ajax({
+        url: '/cohseio/controller/afegirTaller.php',
+        type: 'POST',
+        data: formData,
+        success: function(data){
+          let response = JSON.parse(data);
+          if(response.success){
+            alert('Taller agregado con éxito');
+            $('#modalTaller').modal('hide');
+          } else {
+            alert('Error al agregar el taller: ' + response.error);
+          }
+        },
+        cache: false,
+        contentType: false,
+        processData: false
+      });
+    });
+  });
+  
--- Controller/afegirTaller.php
+++ Controller/afegirTaller.php
@@ -1,11 +1,11 @@
 <?php
-require_once 'connexio.php';
+require_once './connexio.php';
 
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
     $db = connexio(); // Usar la función de conexión que definiste en connexio.php
     
     // Captura de datos del formulario
-    $nom = $_POST['nom'] ?? null; // El operador ?? es una forma abreviada de if (isset($_POST['nom'])) { $nom = $_POST['nom']; } else { $nom = null; } 
+    $nom = $_POST['nom'] ?? null; 
     $professor = $_POST['professor'] ?? null;
     $material = $_POST['material'] ?? null;
     
@@ -14,7 +14,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
     
     if ($nom && $professor && $material) {
         // Preparación de la sentencia SQL para insertar datos
-        $query = $db->prepare("INSERT INTO tallers (nom, professor, material) VALUES (?, ?, ?)");
+        $query = $db->prepare("INSERT INTO tallers (nom, material, professor) VALUES (?, ?, ?)");
         $result = $query->execute([$nom, $professor, $material]);
         
         if($result) {
